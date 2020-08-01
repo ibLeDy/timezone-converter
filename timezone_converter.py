fb892fd955530fdb356bc6553f2698f826bd2444
bLeDy <deejaynof@gmail.com>
2020-08-02 01:30:55 +0200

    Big refactor: delete useless stuff and simplify the whole script...
    - Do not use `aliases.py`, an in-place created dict
      following the same structure is used instead.
    - Use `datetime`'s default methods for formatting
    - Add basic argument parsing
    - README contains a description.

--- timezone_converter.py
+++ timezone_converter.py
@@ -1,95 +1,43 @@
-"""\
-I'm annoyed with the usual webpages that convert timezones
-but only show ONE hour at a time, so i created this script
-to have a simple and quick way of comparing timezones.
-"""
-
 import datetime
-import sys
+from argparse import ArgumentParser
 
 import pytz
 
-from aliases import timezones
+
+def display_timezones(local, foreign, capital):
+    fmt = '%Y-%m-%d %H:%M'
+    length = len(local.strftime(fmt)) + 3
+
+    local_header = 'LOCAL'
+    foreign_header = f'{capital.upper()}'
+    print(local_header.center(length), foreign_header.center(length))
+
+    for hour in range(24):
+        local_plus = (local + datetime.timedelta(hours=hour)).strftime(fmt)
+        foreign_plus = (foreign + datetime.timedelta(hours=hour)).strftime(fmt)
+        print(local_plus.center(length), foreign_plus.center(length))
 
 
-def get_date():
-    today = datetime.datetime.now().astimezone()
-    year, month, day = [int(i) for i in str(today).split(' ')[0].split('-')]
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        description='Compare your local timezone with a foreign one'
+    )
+    parser.add_argument(
+        'timezone',
+        help='foreign timezone that gets compared with your local one',
+    )
 
-    return year, month, day
+    args = parser.parse_args()
+    timezones = {tz.lower().split('/')[-1]: tz for tz in pytz.all_timezones}
 
+    try:
+        timezone = timezones[args.timezone]
+    except KeyError:
+        raise Exception(f'{args.timezone !r} is not an available timezone')
 
-def humanify(local, foreign):
-    local_splitted = str(local).split(' ')
-    local_date = '-'.join(local_splitted[0].split('-'))
-    local_time = ':'.join(local_splitted[1].split(':')[:2])
-    local_string = ' '.join([local_date, local_time])
-
-    foreign_splitted = str(foreign).split(' ')
-    foreign_date = '-'.join(foreign_splitted[0].split('-'))
-    foreign_time = ':'.join(foreign_splitted[1].split(':')[:2])
-    foreign_string = ' '.join([foreign_date, foreign_time])
-
-    return local_string, foreign_string
-
-
-def print_list(local, foreign, length):
-    print('LOCAL'.center(length), city.upper().center(length))
-
-    for i in range(24):
-        local_plus = local + datetime.timedelta(hours=i)
-        foreign_plus = foreign + datetime.timedelta(hours=i)
-
-        local_string, foreign_string = humanify(local_plus, foreign_plus)
-        local_time = local_string.split(' ')[1]
-        foreign_time = foreign_string.split(' ')[1]
-
-        print(local_time.center(length), foreign_time.center(length))
-
-
-def print_list_complete(local, foreign, length):
-    print('LOCAL'.center(length), city.upper().center(length))
-
-    for i in range(24):
-        local_plus = local + datetime.timedelta(hours=i)
-        foreign_plus = foreign + datetime.timedelta(hours=i)
-
-        local_string, foreign_string = humanify(local_plus, foreign_plus)
-
-        print(local_string.center(length), foreign_string.center(length))
-
-
-try:
-    city = sys.argv[1].lower()
-    tz = timezones[city]
-except IndexError:
-    print('Error: City not provided')
-except KeyError:
-    print('Error: City not found')
-else:
-    # Get current date and time
-    year, month, day = get_date()
-
-    # Get local datetime object of today at 00:00
-    local = datetime.datetime(year=year, month=month, day=day).astimezone()
-
-    # Get foreign datetime object of today at 00:00
-    foreign = local.astimezone(pytz.timezone(tz))
-
-    # Get length of local date for output formatting
-    length = len(str(local).split('+')[0])
-
-    # Print full table with dates
-    print_list_complete(local, foreign, length)
-
-    # Print simplified table with only hours
-    # print_list(local, foreign, len(city))
-
-
-# TODO: option to display date (2019-12-30 00:00:00, 2019-12-29 18:00:00)
-# TODO: option to display timezone (00:00:00+01:00, 18:00:00-05:00)
-# TODO: color code
-# TODO: add argument parsing
-# TODO: add verbose
-# TODO: document functions
-# TODO: improve error handling
+    today = datetime.datetime.now()
+    local_midnight = datetime.datetime(
+        today.year, today.month, today.day
+    ).astimezone()
+    foreign_midnight = local_midnight.astimezone(pytz.timezone(timezone))
+    display_timezones(local_midnight, foreign_midnight, args.timezone)
